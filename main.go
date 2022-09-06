5fbd7b5a1d7bcffa8b33f87c8d427c41ada79015
Tav <therzlic@cisco.com>
2022-09-06 11:33:25 +0300

    Added issue event extraction

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/pkg/errors"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	githubToken := os.Getenv("GITHUB_TOKEN")
+	eventName := os.Getenv("GITHUB_EVENT_NAME")
+	fmt.Printf("github token: %s\n", githubToken)
+	fmt.Printf("event name: %s\n", eventName)
+	payload := getIssuesEvent()
+	issueNodeId, err := getIssueNodeId(payload)
+	if err != nil {
+		log.Printf("[ERROR] %+v\n", errors.Wrap(err, "Failed to extract issue id"))
+	}
+	fmt.Printf("issue id: %s\n", issueNodeId)
 }
