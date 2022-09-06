4555b4037aea22f78883b3df78676e9695b65d87
Tav <therzlic@cisco.com>
2022-09-06 12:16:23 +0300

    Changed go run script

---
+++ process_github_event.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/google/go-github/v47/github"
+	"github.com/pkg/errors"
+)
+
+func getIssuesEvent() github.IssuesEvent {
+	var jsonFilePath string = os.Getenv("GITHUB_EVENT_PATH")
+
+	jsonFile, err := os.Open(jsonFilePath)
+	if err != nil {
+		log.Printf("[ERROR] %+v\n", errors.Wrap(err, "Failed to open json"))
+	}
+	defer jsonFile.Close()
+
+	// read opened jsonFile as a byte array.
+	jsonByte, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		log.Printf("[ERROR] %+v\n", errors.Wrap(err, "Failed to read json as a byte array"))
+	}
+
+	payload := github.IssuesEvent{}
+	err = json.Unmarshal(jsonByte, &payload)
+	if err != nil {
+		log.Printf("[ERROR] %+v\n", errors.Wrap(err, "Failed to unmarshal JSON to Go Object"))
+	}
+
+	return payload
+}
+
+func getIssueNodeId(event github.IssuesEvent) (string, error) {
+	issueNodeId := event.GetIssue().GetNodeID()
+
+	if issueNodeId == "" {
+		return "", errors.New("Issue ID is \"\". Failed to get issue id properly")
+	}
+
+	return issueNodeId, nil
+}
